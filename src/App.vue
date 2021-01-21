5892bc0df7ec4cf091e04f33c0a60d62491dc2ad
= <=>
2021-01-21 21:16:13 +0200

    Adding Auth Test States

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,44 @@
 <template>
-  <base-container title="Vuex"></base-container>
+<div>
+  <base-container title="Vuex" v-if="isAuth">
+  <the-counter></the-counter>
+  <favorite-value></favorite-value>
+    <button @click="addOne">Add 1</button>
+    <change-counter></change-counter>
+  </base-container>
+
+  <base-container title="Auth">
+    <user-auth></user-auth>
+  </base-container>
+  </div>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
+import ChangeCounter from './components/ChangeCounter.vue'
+import FavoriteValue from './components/FavoriteValue.vue'
+import UserAuth from './components/UserAuth.vue'
 
 export default {
   components: {
     BaseContainer,
+    TheCounter,
+    ChangeCounter,
+    FavoriteValue,
+    UserAuth
   },
+ 
+  methods: {
+    addOne(){
+      this.$store.dispatch('increase', {value: 20});
+    }
+  },
+  computed:{
+        isAuth(){
+            return this.$store.getters.userIsAuth
+        }
+    }
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,67 @@
 import { createApp } from 'vue';
+import { createStore } from 'vuex';
 
 import App from './App.vue';
 
+const store = createStore({
+    state(){
+        return{
+            counter: 0,
+            isLoggedIn: false
+        }
+    },
+    mutations: {
+        increment(state){
+            state.counter++;
+        },
+        increase(state, payload){
+            state.counter = state.counter + payload.value;
+        },
+        setAuth(state, payload){
+            state.isLoggedIn = payload.isAuth
+        }
+    },
+    actions: {
+        increment(context){
+            setTimeout(function(){
+                context.commit('increment')
+            })    
+        },
+        increase(context, payload){
+            setTimeout(function(){
+                context.commit('increase', payload)
+            }, 2000)
+        },
+        login(context){
+            context.commit('setAuth', {isAuth: true})
+        },
+        logout(context){
+            context.commit('setAuth', {isAuth: false})
+        }
+    },
+    getters: {
+        finalCounter(state){
+            return state.counter * 2;
+        },
+        normaliziedCounter(_,getters){
+            if(getters.finalCounter < 0){
+                return 0
+            }
+
+            if(getters.finalCounter > 100){
+                return 100
+            }
+
+            return getters.finalCounter
+        },
+        userIsAuth(state){
+            return state.isLoggedIn
+        }
+    }
+});
+
 const app = createApp(App);
+
+app.use(store);
 
 app.mount('#app');
